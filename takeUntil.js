42fe2d2b11650b0048a2638d8a61ec2933edfc9a
Christy Lum <christy.lum121@gmail.com>
2020-08-01 23:21:15 +0000

    Completed code for takeUntil.js, added instructions to findKey.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,18 +1,36 @@
+//TEST CODE
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
 const takeUntil = function(array, callback) {
-  // ...
+  const resultsArr = [];
+
+  for (let item of array) {
+    //console.log('loop works');
+    if (callback(item)) {
+      //console.log(item);
+      resultsArr.push(item);
+    } else {
+      return resultsArr;
+    }
+  }
+  return resultsArr;
 }
 
-//TEST CODE
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+const callback = function(item) {
+  if (item < 0 || item === ',') {
+    return false;
+  } else {
+    return true;
+  }
+}
 
+console.log(takeUntil(data1, callback));
 console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
+console.log(takeUntil(data2, callback))
 
 /*
 
