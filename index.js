37bcc15e4ec373d29f39e33b62bf2fddd2ffedc6
Christy Lum <christy.lum121@gmail.com>
2020-08-02 23:42:40 +0000

    Refactored index.js using property value shorthand

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+
+module.exports = {
+  head, 
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays
+}
+
 
 
