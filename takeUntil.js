788824898e12967f7df3a04ef1f45f16f322fa44
Christy Lum <christy.lum121@gmail.com>
2020-08-03 00:04:55 +0000

    Cleaned up comments for takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,3 @@
-//TEST CODE
-// const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);
-
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 
